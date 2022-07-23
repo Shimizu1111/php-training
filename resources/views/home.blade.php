725620f3dd738b97dc42f3c178e37af65be75183
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-07-23 18:37:38 +0900

    20220728課題(Signin,Signup,Blog)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,5 @@
 <p>ログインID: {{$login_user->id}}</p>
 <p>ログインユーザー: {{$login_user->name}}</p>
 
-<p>{{ $password[0]->password }}</p>
 <p><a href="/user/profile/{{$login_user->id}}/{{$password[0]->password}}">プロフィール画面</a></p>
+<p><a href="/blog">Blog画面</a></p>
